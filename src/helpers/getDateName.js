ce67ccf8e5dfe4714d1f900691c2fab8233e5266
Alena <olenanepokryta1997@gmail.com>
2023-07-14 12:41:05 +0300

    feat: add functions for get current week and name date

--- src/helpers/getDateName.js
+++ src/helpers/getDateName.js
@@ -1,13 +1,13 @@
-function getDateInfo(date) {
+function getDateName(date) {
   const dateObj = new Date(date);
   const weekDayShort = dateObj.toLocaleString('en-US', { weekday: 'short' });
   const monthNameLong = dateObj.toLocaleString('en-US', { month: 'long' });
   const monthName = dateObj.toLocaleString('en-US', { month: 'short' });
   const monthDay = dateObj.getDate();
-    
+
   return {
     monthName, weekDayShort, monthNameLong, monthDay 
   };
 }
 
-export default getDateInfo;
+export default getDateName;
