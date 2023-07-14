75cfed57b9445740cf38aeffccb57fa52f1ecea1
Alena <olenanepokryta1997@gmail.com>
2023-07-14 12:38:09 +0300

    fix: fix functions in App component

--- src/App.js
+++ src/App.js
@@ -1,108 +1,117 @@
-import React, {
-  useEffect, useMemo, useState 
-} from 'react';
+import React, { useEffect, useState } from 'react';
+import getCurrentWeekNumber from './helpers/getCurrentWeekNumber';
 
 import Header from './components/Header/Header';
 import Week from './components/Week/Week';
-
 import Loading from './assets/img/waiting.gif';
 import Footer from './components/Footer/Footer';
 
 function App() {
-  const [weekData, setWeekData] = useState(null);
+  const [currWeek, setCurrWeek] = useState(null);
+  const [currWeekData, setCurrWeekData] = useState(null);
   const [errorData, setErrorData] = useState(null);
-  const [activeIndex, setActiveIndex] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://api.npoint.io/695f083b025fc7c7c739');
-        const data = await response.json();
-        setWeekData(data);
-      } catch (error) {
-        console.log('Error', error);
-        setErrorData(true);
-      }
-    };
-    fetchData();
+    setCurrWeek(getCurrentWeekNumber());
   }, []);
 
-  const filterWeekData = useMemo(() => {
-    if (!weekData?.length) {
-      return [];
+  const filterWeekData = (data, selectedWeek) => {
+    if (!data.days.length) {
+      return { days: [] };
     }
-    const weekDataToReturn = JSON.parse(JSON.stringify(weekData));
-  
-    const today = new Date();
-    // const today = new Date('July 13, 2023 10:24:00');
+    const realCurrWeek = getCurrentWeekNumber();
+
+    if (realCurrWeek > selectedWeek) {
+      // очистить время
+      return {
+        ...data,
+        days: data.days.map((d) => ({
+          date: d.date,
+          time: [],
+        }))
+      };
+    } if (realCurrWeek < selectedWeek) {
+      return data;
+    }
+    const weekDataToReturn = JSON.parse(JSON.stringify(data));
+    
+    // const today = new Date();
+    const today = new Date('July 12, 2023 10:24:00');
+    const timeNow = today.toTimeString().split(' ')[0];
     const todayDate = `${today.getFullYear()}-${(today.getMonth() + 1) < 10 
       ? `0${today.getMonth() + 1}` : (today.getMonth() + 1)}-${today.getDate() < 10 
       ? `0${today.getDate()}` : today.getDate()}`;
 
-    const timeNow = today.toTimeString().split(' ')[0];
-
-    for (let i = 0; i < weekDataToReturn.length; i += 1) {
-      const currentWeek = weekDataToReturn[i];
-
-      for (let j = 0; j < currentWeek.length; j += 1) {
-        const currentDay = currentWeek[j];
-        if (currentDay.date === todayDate) {
-          currentDay.time = currentDay.time.filter((item) => item >= timeNow);
-          setActiveIndex(i);
-        }
-        if (currentDay.date < todayDate) {
-          currentDay.time = [];
-        }
+    for (let i = 0; i < weekDataToReturn.days.length; i += 1) {
+      const currentDay = weekDataToReturn.days[i];
+      if (currentDay.date === todayDate) {
+        currentDay.time = currentDay.time.filter((item) => item >= timeNow);
       }
-        
-      //   for (let j = 0; j < currentWeek.length; j += 1) {
-      //     console.log(currentWeek[j], 'currentWeek');
-      //     const currentDay = currentWeek[j];
-
-      //     if (currentDay.date <= todayDate) {
-      //       currentDay.time = [];
-      //       //   continue;
-      //     }
-  
-    //     // currentDay.time = currentDay.time.filter((item) => item >= timeNow);
-    //   }
+      if (currentDay.date < todayDate) {
+        currentDay.time = [];
+      }
     }
+      
     return weekDataToReturn;
-  }, [weekData]);
+  };
+
+  useEffect(() => {
+    if (!currWeek) {
+      return;
+    }
+    setIsLoading(true);
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/weeks/${currWeek}`);
+        // const response = await fetch('https://api.npoint.io/695f083b025fc7c7c739/$4');
+        if (!response.ok) {
+          throw new Error(`Error:${response.status}`);
+        }
+        const data = await response.json();
+        setCurrWeekData(filterWeekData(data, currWeek));
+      } catch (error) {
+        console.error('Error', error);
+        setErrorData(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    setTimeout(() => {
+      fetchData();
+    }, 1000);
+  }, [currWeek]);
 
   const showPrevWeek = () => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setActiveIndex((prevIndex) => (prevIndex === 0 ? 0 : prevIndex - 1));
-      setIsLoading(false);
-    }, 300);
+    setCurrWeek((prevIndex) => (prevIndex - 1));
   };
 
   const showNextWeek = () => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setActiveIndex((prevIndex) => (prevIndex >= weekData.length - 1 ? weekData.length - 1 : prevIndex + 1));
-      setIsLoading(false);
-    }, 300);
+    setCurrWeek((prevIndex) => (prevIndex + 1));
   };
 
-  if (!weekData) {
-    return <img alt="Loading" src={Loading} className="loading" />;
-  }
-
   if (errorData) {
-    return <div>`We&apos;re very sorry for the error.`</div>;
+    return <h2 className="error_message">We&apos;re very sorry for the error.</h2>;
   }
 
   return (
-    <div className="wrapper">
-      <Header showNextWeek={showNextWeek} showPrevWeek={showPrevWeek} filterWeekData={filterWeekData[activeIndex]} />
-      {isLoading 
-        ? (<img alt="Loading" src={Loading} className="loading" />) 
-        : (<Week filterWeekData={filterWeekData[activeIndex]} />)}
-      <Footer />
-    </div>
+    (currWeekData !== null) ? (
+      <div className="wrapper">
+        <Header
+          showNextWeek={showNextWeek} 
+          showPrevWeek={showPrevWeek}
+          daysCurrWeekData={currWeekData.days}
+        />
+        <Week daysCurrWeekData={currWeekData.days} key={currWeekData.id} />
+        {isLoading && (
+          <div className="spinner-container">
+            <img alt="Loading" src={Loading} className="loading" />
+            <div className="overlay" />
+          </div>
+        )}
+        <Footer />
+      </div>
+    ) : <img alt="Loading" src={Loading} className="loading" />
   );
 }
 
